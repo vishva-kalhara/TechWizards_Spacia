4d3fee48c8aeb1196f1795215b687018dd2cb86c
vishva-kalhara <vishvakalhara@gmail.com>
2023-08-15 22:33:38 +0530

    Implemented Stream ✅

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -30,24 +30,27 @@ import 'package:spacia/models/user.dart';
 // }
 
 class AuthService {
-  // final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // Create User object based on Firebase User
-  UserModel _userFromFirebaseUser(String userUid) {
-    return UserModel(uid: userUid);
+  UserModel? _userFromFirebaseUser(User? user) {
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
 
-    // return userUid != null ? UserModel(uid: userUid);
+  // Crete the stream for check the suth changes in the user
+  Stream<UserModel?> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
   }
 
   // Anonimously
   Future signInAnon() async {
     try {
-      final userCredential = await FirebaseAuth.instance.signInAnonymously();
-      // var res=
-      // User s1 = userCredential.user?;
-      var user = userCredential.user;
-      var uid = user?.uid;
-      return _userFromFirebaseUser(uid!);
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      // var user = userCredential.user;
+      // var uid = user?.uid;
+      return _userFromFirebaseUser(user);
+      // return _userFromFirebaseUser(uid!);
     } catch (e) {
       print(e.toString());
       return null;
